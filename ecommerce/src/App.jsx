f234e30522da7c31308ffa5756076958e0d5fd2f
phanindra2212 <phanindrayarajarla2005@gmail.com>
2026-07-03 22:29:11 +0530

    all functionality aded

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,18 +1,21 @@
-import {HomePage } from './pages/HomePage'
-import { Route, Routes } from 'react-router'
-import './App.css'
-import { CheckoutPage } from './pages/CheckoutPages'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPages';
+import { OrdersPage } from './pages/OrdersPage.jsx';
+import { Tracking } from './pages/Tracking.jsx';
+import './App.css';
 
 function App() {
-
   return (
-    <Routes>
-       <Route path = "/" element={<HomePage/>}></Route>
-        <Route path = "checkout" element = {<CheckoutPage/>}>
-
-        </Route>
-   </Routes>
-   )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} /> 
+        <Route path="orders" element={<OrdersPage />} /> 
+        <Route path="tracking" element={<Tracking />} /> 
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
