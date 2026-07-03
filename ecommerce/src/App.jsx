1b3a2c4809131dbdfbde5cbb133744bbf5169d35
phanindra2212 <phanindrayarajarla2005@gmail.com>
2026-07-03 14:53:31 +0530

    create routing

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,21 @@
 import {HomePage } from './pages/HomePage'
+import { Route, Routes } from 'react-router'
 import './App.css'
 
 function App() {
 
   return (
-   <HomePage/>
-  )
+    <Routes>
+       <Route path = "/" element={<HomePage/>}></Route>
+        <route path = "checkout" element = {
+          <div>
+            Test checkout page
+          </div>
+        }>
+          
+        </route>
+   </Routes>
+   )
 }
 
 export default App
